7b1735070491715e36a777698c7543a6eae2ea16
Irfan Marzuki <ragnaboyx@gmail.com>
2019-01-30 09:50:27 +0700

    add basic locale service

---
+++ src/utils/i18n/i18nProvider.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18n';
+
+import fs from 'fs';
+import path from 'path';
+import { I18nProvider } from './LocaleService';
+
+const localesPath = path.join(__dirname, '..', '..', '..', 'config', 'locales');
+const files = fs.readdirSync(localesPath);
+
+i18n.configure({
+  locales: files.map(file => path.basename(file)),
+  defaultLocale: 'default',
+  directory: localesPath,
+  objectNotation: true
+});
+
+const i18nProvider = i18n as any;
+
+i18nProvider.translate = i18n.__mf;
+
+export default i18nProvider as I18nProvider;

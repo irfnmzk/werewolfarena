c9de9b181799bac9cf946d5bf98b64617ef5a7b1
Irfan Marzuki <ragnaboyx@gmail.com>
2019-02-16 09:01:34 +0700

    update message

--- src/tests/scenario/GameScenario.ts
+++ src/tests/scenario/GameScenario.ts
@@ -35,7 +35,7 @@ function handleScene(scene: time, day: any, player: Player[]) {
     case 0:
       switch (scene) {
         case 'NIGHT':
-          user[4].eventCallback(scene, ge('shoot', player[0].userId));
+          // user[4].eventCallback(scene, ge('shoot', player[0].userId));
           break;
         case 'DUSK':
           // user[0].eventCallback(scene, ge('vote', player[4].userId));
@@ -44,11 +44,11 @@ function handleScene(scene: time, day: any, player: Player[]) {
       break;
     case 1:
       switch (scene) {
-        case 'NIGHT':
-          // user[4].eventCallback(scene, ge('bite', player[2].userId));
+        case 'DAY':
+          user[4].eventCallback(scene, ge('shoot', player[2].userId));
           break;
         case 'DUSK':
-          user[3].eventCallback(scene, ge('vote', player[1].userId));
+          // user[3].eventCallback(scene, ge('vote', player[1].userId));
           break;
       }
       break;
